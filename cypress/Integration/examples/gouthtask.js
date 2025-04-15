5d33664ce833d5c06404a39788ec4c0e5c01e61b
KesavMadugula <madugulakesav@gmail.com>
2025-04-15 17:15:06 +0530

    goutham task

---
+++ cypress/Integration/examples/gouthtask.js
@@ -0,0 +1,15 @@
+describe('API Testing Menu', () => {
+    it('Click API Testing using .each()', () => {
+
+      
+      cy.visit('https://automationexercise.com/');
+      cy.wait(2000);
+      cy.get('ul.nav.navbar-nav > li').each(($el) => {
+        const menuText = $el.text().trim();
+        if (menuText.includes('API Testing')) {
+          cy.wrap($el).click();
+        }
+      });
+      cy.url().should('include', '/api_list');
+    });
+  });
